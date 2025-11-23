89d71120e886d6acf77b8745d5ec34a0e327f4f6
emtu5 <115076076+emtu5@users.noreply.github.com>
2025-11-23 20:19:31 +0200

    card holders spawn card slots

--- Assets/Scripts/CardHolder.cs
+++ Assets/Scripts/CardHolder.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class CardHolder : MonoBehaviour
 {
+    public int cardSlots = 0;
+    public CardSlot cardSlotPrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for (int i = 0; i < cardSlots; i++)
+        {
+            CardSlot cSlot = Instantiate(cardSlotPrefab, gameObject.transform);
+        }
     }
 
     // Update is called once per frame
