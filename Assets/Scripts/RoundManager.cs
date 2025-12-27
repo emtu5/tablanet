f60f309f97f8382ad8799b35f6820a9872850a56
emtu5 <115076076+emtu5@users.noreply.github.com>
2025-12-27 02:52:42 +0200

    let's not Int32.Parse every single time

--- Assets/Scripts/RoundManager.cs
+++ Assets/Scripts/RoundManager.cs
@@ -11,6 +11,12 @@ public class RoundManager : MonoBehaviour
     public TMP_Text mults;
     public TMP_Text hand;
     public TMP_Text total;
+    public int rnd;
+    public int scoreReq;
+    public int ptns;
+    public int mult;
+    public int handVal;
+    public int totl;
     public Button playButton;
     public GameObject winPanel;
     public GameObject losePanel;
@@ -23,23 +29,28 @@ public class RoundManager : MonoBehaviour
     
     void Start()
     {
-        Debug.Log(GameManager.Instance.round);
-        round.text = GameManager.Instance.round.ToString();
+        // Debug.Log(GameManager.Instance.round);
+        rnd = GameManager.Instance.round;
+        round.text = rnd.ToString();
         Debug.Log("mrow");
         
-        Debug.Log(round.text);
-        scoreRequirement.text = GameManager.Instance.scoreRequirement[Int32.Parse(round.text)].ToString();
+        // Debug.Log(round.text);
+        scoreReq = GameManager.Instance.scoreRequirement[rnd];
+        scoreRequirement.text = scoreReq.ToString();
     }
 
     public void ShowHandValue(int pts, int mlt) {
-        int handValue = pts * mlt;
+        ptns = pts;
+        mult = mlt;
+        handVal = ptns * mult;
         points.text = pts.ToString();
         mults.text = mlt.ToString();
-        hand.text = handValue.ToString();
+        hand.text = handVal.ToString();
     }
 
     public void AddToTotal() {
-        total.text = (Int32.Parse(total.text) + Int32.Parse(hand.text)).ToString();
+        totl += handVal;
+        total.text = totl.ToString();
         CheckWin();
     }
 
@@ -59,19 +70,19 @@ public class RoundManager : MonoBehaviour
 
     public void CheckWin()
     {
-        if (Int32.Parse(total.text) >= Int32.Parse(scoreRequirement.text)) // win
+        if (totl >= scoreReq) // win
         {
             playButton.interactable = false;
             GameManager.Instance.advanceToNextRound = true;
             GameManager.Instance.AdvanceRound();
-            winPanel.GetComponentInChildren<TMP_Text>().text = String.Format("Round {0} Complete!", round.text);
+            winPanel.GetComponentInChildren<TMP_Text>().text = String.Format("Round {0} Complete!", rnd);
             winPanel.SetActive(true);
         }
         else if (handsLeft == 0) // lose
         {
             GameManager.Instance.advanceToNextRound = true;
             GameManager.Instance.LoseGame();
-            losePanel.GetComponentInChildren<TMP_Text>().text = String.Format("Game Over! Final Round: {0}", round.text);
+            losePanel.GetComponentInChildren<TMP_Text>().text = String.Format("Game Over! Final Round: {0}", rnd);
             losePanel.SetActive(true);
         }
     }
